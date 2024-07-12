591e99323817db992cf99d247a10b30cd98d5e16
Guitar <kittipong.sataya@gmail.com>
2024-07-12 16:18:15 +0700

    fix

--- src/constants/index.ts
+++ src/constants/index.ts
@@ -285,7 +285,7 @@ const projects: TProject[] = [
     sourceCodeLink: "https://github.com/",
   },
   {
-    name: "Job IT",
+    name: "Job IT LOREM",
     description:
       "Web application that enables users to search for job openings, view estimated salary ranges for positions, and locate available jobs based on their current location.",
     tags: [
@@ -306,7 +306,7 @@ const projects: TProject[] = [
     sourceCodeLink: "https://github.com/",
   },
   {
-    name: "Trip Guide",
+    name: "Trip Guide LOREM",
     description:
       "A comprehensive travel booking platform that allows users to book flights, hotels, and rental cars, and offers curated recommendations for popular destinations.",
     tags: [
